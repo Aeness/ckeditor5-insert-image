3c33fa2356ce5b68062b593456631c04979b85cd
Aeness <dev@aeness.net>
2020-05-19 11:55:05 +0200

    Add focus life cycler to the form.

--- src/ui/insertimageformview.js
+++ src/ui/insertimageformview.js
@@ -7,6 +7,10 @@ import InputTextView from '@ckeditor/ckeditor5-ui/src/inputtext/inputtextview';
 import submitHandler from '@ckeditor/ckeditor5-ui/src/bindings/submithandler';
 import KeystrokeHandler from '@ckeditor/ckeditor5-utils/src/keystrokehandler';
 
+import ViewCollection from '@ckeditor/ckeditor5-ui/src/viewcollection';
+import FocusTracker from '@ckeditor/ckeditor5-utils/src/focustracker';
+import FocusCycler from '@ckeditor/ckeditor5-ui/src/focuscycler';
+
 import checkIcon from '@ckeditor/ckeditor5-core/theme/icons/check.svg';
 import cancelIcon from '@ckeditor/ckeditor5-core/theme/icons/cancel.svg';
 import '../../theme/insertimageform.css';
@@ -22,8 +26,19 @@ export default class InsertImageFormView extends View {
 
         /**
          * Tracks information about keystrokes in the form.
+         * It will be used by the action form to close the UI.
          */
         this.keystrokes = new KeystrokeHandler();
+
+        /**
+         * A collection of views that can be focused in the form.
+         */
+        this._focusables = new ViewCollection();
+
+        /**
+         * Tracks information about DOM focus in the form.
+         */
+        this._focusTracker = new FocusTracker();
 
 
         // Creates a new collection of views, which can be used as children of this view.
@@ -62,7 +77,8 @@ export default class InsertImageFormView extends View {
 
     /**
      * Creates an input view (without label)
-     * and add it to the children of the form.
+     * and add it to the children of the form,
+     * and add it to the focusables and focusTracker.
      */
     _createUrlInput() {
         const labeledInput = new LabeledInputView( this.locale, InputTextView );
@@ -71,13 +87,16 @@ export default class InsertImageFormView extends View {
 
         this._children.add( labeledInput );
         // TODO add label like plugin media (to be consistent ), or not to be consistent with link ??
+        this._focusables.add( labeledInput );
+        this._focusTracker.add( labeledInput.element );
 
         return labeledInput;
     }
 
     /**
      * Creates a button view
-     * and add it to the children of the form.
+     * and add it to the children of the form,
+     * and add it to the focusables and focusTracker.
      */
     _createButton( label, icon, className, eventName ) {
         const button = new ButtonView( this.locale );
@@ -100,6 +119,8 @@ export default class InsertImageFormView extends View {
         }
 
         this._children.add( button );
+        this._focusables.add( button );
+        this._focusTracker.add( button.element );
 
         return button;
     }
@@ -120,6 +141,18 @@ export default class InsertImageFormView extends View {
         // Start listening for the keystrokes coming from #element.
         // for escape
         this.keystrokes.listenTo( this.element );
+
+        // Cycling over focusable element
+        const cycler = new FocusCycler( {
+            focusables : this._focusables, focusTracker: this._focusTracker,
+            keystrokeHandler: this.keystrokes,
+            actions: {
+                // focusPrevious() will be called on the cycler.
+                focusPrevious: 'shift + tab',
+                // focusNext() will be called on the cycler.
+                focusNext: 'tab'
+            }
+        } );
     }
 
     /**
